327d57c6be34c8bd929fc846b134386021a50bf7
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-03-04 20:31:27 +0100

    Test dynamodb
    Still not working

--- api-service/src/common/dynamodb.ts
+++ api-service/src/common/dynamodb.ts
@@ -8,4 +8,12 @@ if (process.env.IS_OFFLINE) {
     };
 }
 
+if (process.env.JEST_WORKER_ID) {
+    options = {
+        region: "local-env",
+        endpoint: "http://localhost:8000",
+        sslEnabled: false
+    };
+}
+
 export default new DynamoDB(options);
---
+++ api-service/tests/postProduct.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@jest/globals'
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { postProduct } from "../src/endpoints/postProduct";
+
+import { Product } from "../src/types/Product";
+
+test('Post poroduct successful', async () => {
+    const product = new Product('1', 'Adidas', 'Shoes', 13090, 2);
+
+    const event = {
+        body: JSON.stringify(product),
+        path: '/product',
+        pathParameters: null,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*'
+        }
+    } as unknown as APIGatewayProxyEvent;
+
+    const response = await postProduct(event);
+    expect(response.statusCode).toEqual(200);
+});
