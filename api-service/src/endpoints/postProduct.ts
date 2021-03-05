782a2842b2294c75387571643e8055315b85724f
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-03-05 08:45:37 +0100

    Refactor

--- api-service/src/endpoints/postProduct.ts
+++ api-service/src/endpoints/postProduct.ts
@@ -1,6 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Callback, Context, Handler } from 'aws-lambda';
 import { DataMapper } from '@aws/dynamodb-data-mapper';
-import { DynamoDB } from 'aws-sdk';
 
 import { Product } from '../types/Product';
 import { Responses } from '../common/responses';
@@ -15,12 +14,12 @@ export const handler: Handler = async (event: APIGatewayProxyEvent, _context: Co
   }
 }
 
-export const postProduct = async (event: APIGatewayProxyEvent, dynamodb: DynamoDB = DynamoDBClient): Promise<APIGatewayProxyResult> => {
+export const postProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   if (!event.body || !(JSON.parse(event.body) as Product)) {
     return Responses._400({ message: `Missing body from request.` });
   }
 
-  const mapper = new DataMapper({ client: dynamodb });
+  const mapper = new DataMapper({ client: DynamoDBClient });
   
   const parsedBody: Product = JSON.parse(event.body);
   const product = new Product(parsedBody.ID, parsedBody.name, parsedBody.description, parsedBody.price, parsedBody.quantity);
